69d29615e7dc52b2ceaf4b2076635726bc1c5f06
NGAN DAM THANH <1821050429@student.humg.edu.vn>
2021-03-12 11:10:10 +0700

    Add project files.

---
+++ CheckLogin/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using CheckLogin.Models;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace CheckLogin.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ViewResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(AccountModel acc, string ReturnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (acc.Username == "admin" && acc.Password == "123123")
+                {
+                    FormsAuthentication.SetAuthCookie(acc.Username, true);
+                    return RedirectToLocal(ReturnUrl);
+                }
+            }
+            return View(acc);
+        }
+        // Hàm đăng xuất khỏi chương trình
+        public ActionResult Logoff()
+        {
+            FromsAuthentication.SignOut();
+            return RedirectToLocal("Index", "Home");
+        }
+    }
+    private ActionResult RedirectToLocal(string returnUrl)
+    {
+        if (Url.IsLocalUrl(returnUrl))
+        {
+            return Redirect(returnUrl);
+        }
+        else
+        {
+            return RedirectToAction("Index", "Home");
+
+        }
+    }
+}
+
+
+
+
+
+
+
+                    
---
+++ CheckLogin/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CheckLogin.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous] // đối với action cho phép truy cập không cần kiểm tra
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //Muốn kiểm tra đăng nhập thì viết Authori/ có thể kiểm tra cả controller
+        [Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [Authorize]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
